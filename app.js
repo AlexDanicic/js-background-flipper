47c979a8b141c0b912a934204a630db5e109a7b3
Alex. Danicic <alex.danicic@gmail.com>
2020-07-23 22:35:27 +0100

    js main logic and function added

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// target the button
+const btn = document.getElementById("btn");
+// target the color
+const color = document.querySelector(".color");
+
+// function that change the background color
+btn.addEventListener("click", function () {
+  // target the body
+  //   console.log(document.body);
+  // get random number between 0 and 3, invoke the function
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  //   change the value
+  color.textContent = colors[randomNumber];
+});
+
+// random number function
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
